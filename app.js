6e0bc007f1be9a1a48de94ed3bc753b2d5b7768c
CrisitianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-10-21 08:52:15 -0500

    feat: :tada: Configuracion Inicial

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
+
+const error = require('./server/middleware/errorHandler');
+
+
+const privateKey = fs.readFileSync('./private.key');
+const certificate = fs.readFileSync('./certificate.crt');
+const app = express();
+
+
+app.use(express.json());
+app.use(error.jsonParseErrorHandler);
+
+
+app.get('/', (req, res) => {
+    res.send('Ok')
+})
+
+const httpsServer = https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app);
+
+const config = {
+    host: process.env.EXPRESS_HOST,
+    port: process.env.EXPRESS_PORT,
+}
+
+httpsServer.listen(3000, () => {
+    console.log('https://localhost:3000');
+});
