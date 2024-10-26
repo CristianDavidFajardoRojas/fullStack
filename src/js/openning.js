6f1443f3f78c04798a541c8edb10ba84f6f09ae4
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-10-26 15:28:34 -0500

    style: :lipstick: Openning LayOut

---
+++ src/js/openning.js
@@ -0,0 +1,7 @@
+document.querySelector('.login-btn').addEventListener('click', function() {
+    alert('Login button clicked');
+});
+
+document.querySelector('.signup-btn').addEventListener('click', function() {
+    alert('Sign Up button clicked');
+});
