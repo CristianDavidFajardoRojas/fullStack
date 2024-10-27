018abab981ec206ecb424bf5813d08d50ce24eac
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-10-27 12:17:54 -0500

    feat: :sparkles: Peticion Login

--- src/js/login.js
+++ src/js/login.js
@@ -1,12 +1,36 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+const uri = `${location.href}`;
+
+document.getElementById('loginForm').addEventListener('submit', async (event) => {
     event.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     
-    console.log('Login attempt:', { email, password });
+    let data = {};
+    data.email = email;
+    data.password = password;
+
+    console.log(data)
+    console.log(JSON.stringify(data))
+
+    let config = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'x-version': '1.0.0'  
+        },
+        body: JSON.stringify(data)
+    };
+
+    let peticion = await fetch(uri, config);
+    let res = await peticion.json();
+    alert(res.message);
+    if(res.status == 200) {
+        alert(res.status);
+        location.href = "/notes";
+    }
+     
     
- 
-    alert('Login successful!');
+
        
    
 });
