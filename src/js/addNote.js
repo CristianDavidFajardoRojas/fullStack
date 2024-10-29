3c5cbd28de7549ab14d5eea75edffa237205a1b0
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-10-28 21:47:46 -0500

    feat: :sparkles: Insertar Nota

--- src/js/addNote.js
+++ src/js/addNote.js
@@ -1,15 +1,16 @@
+const uriWindows = `${location.href}`;
+const uri = uriWindows.split('/notes')[0] + '/notes';
 
 const backBtn = document.getElementById('backBtn');
 const previewBtn = document.getElementById('previewBtn');
 const imageBtn = document.getElementById('imageBtn');
 const titleInput = document.querySelector('.title-input');
 const contentInput = document.querySelector('.content-input');
+const modal = document.getElementById('modal');
+const saveBtn = document.querySelector('.save');
 
 backBtn.addEventListener('click', () => {
-    if (confirm('Are you sure you want to go back? Any unsaved changes will be lost.')) {
-        // Handle going back (e.g., window.history.back())
-        console.log('Going back...');
-    }
+    location.href = '/notes'
 });
 
 previewBtn.addEventListener('click', () => {
@@ -17,18 +18,44 @@ previewBtn.addEventListener('click', () => {
     console.log('Preview clicked');
 });
 
-imageBtn.addEventListener('click', () => {
-    // Handle image insertion
-    console.log('Image insertion clicked');
+imageBtn.addEventListener('click', async() => {
+    let dataInsert = {
+        title: titleInput.value.trim(),
+        description: contentInput.value.trim()
+    }
+    if(dataInsert.title !== '' && dataInsert.description !== ''){
+        let configInsert = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'x-version': '1.0.0'  
+            },
+            body: JSON.stringify(dataInsert)
+        };
+    
+        console.log(JSON.stringify(dataInsert))
+        let peticionInsert = await fetch(`${uri}` , configInsert);
+        let resInsert = await peticionInsert.json(); 
+        
+        if(resInsert.status == 201){
+            modal.style.display = 'block';
+            saveBtn.addEventListener('click', ()=>{
+                location.href = '/notes'
+            })
+        }
+        
+    }
+
 });
 
-// Example of applying formatting (bold) when the B button is clicked
-document.querySelector('.formatting-bar button:first-child').addEventListener('click', () => {
-    document.execCommand('bold', false, null);
-});
 
 // Adjust textarea height based on content
 contentInput.addEventListener('input', function() {
+    this.style.height = 'auto';
+    this.style.height = this.scrollHeight + 'px';
+});
+
+titleInput.addEventListener('input', function() {
     this.style.height = 'auto';
     this.style.height = this.scrollHeight + 'px';
 });
