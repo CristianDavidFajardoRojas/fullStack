cdca179f110d3b52b4df6710206b917788eb23d5
CristianDavidFajardoRojas <cfajardorojas98@gmail.com>
2024-10-28 22:12:49 -0500

    feat: :sparkles: Modal Login

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,9 @@
 const uri = `${location.href}`;
 
+const modal = document.getElementById('modal');
+const saveBtn = document.querySelector('.save');
+const textModal = document.querySelector('.text-model');    
+
 document.getElementById('loginForm').addEventListener('submit', async (event) => {
     event.preventDefault();
     const email = document.getElementById('email').value;
@@ -20,10 +24,19 @@ document.getElementById('loginForm').addEventListener('submit', async (event) =>
 
     let peticion = await fetch(uri, config);
     let res = await peticion.json();
-    alert(res.message);
-    if(res.status == 200) {
-        location.href = "/notes";
-    }
+    
+
+    console.log(textModal)
+    textModal.textContent = res.message
+    modal.style.display = 'block';
+
+    saveBtn.addEventListener('click', ()=> {
+        if(res.status == 200) {
+            location.href = "/notes";
+        }else {
+            modal.style.display = 'none';
+        }
+    })
      
     
 
