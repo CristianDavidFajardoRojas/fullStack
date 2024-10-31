36807f184460cef840ee0add9758083ca778359d
cris-fr <cfajardorojas98@gmail.com>
2024-10-30 19:12:17 -0500

    feat: :rewind: Revert Config

--- server/middleware/sessionConfig.js
+++ server/middleware/sessionConfig.js
@@ -6,14 +6,6 @@ const fs = require('fs');
 module.exports = session({
     secret: process.env.EXPRESS_SECRET_KEY,
     resave: false,
-    saveUninitialized: false, // Solo guarda sesiones inicializadas
-    cookie: {
-      secure: process.env.NODE_ENV === 'production', // Usa true en producción
-      httpOnly: true, // No accesible desde JavaScript
-      sameSite: 'Strict', // Cambia a 'Lax' si tienes problemas
-      maxAge: 1800000 // Duración de la cookie
-    }
-  })
-
-
-
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 1800000 },
+})
