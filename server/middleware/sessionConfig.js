e89d0d36879d0f6927f82f4cc7ee4096a09b0508
cris-fr <cfajardorojas98@gmail.com>
2024-10-31 16:44:07 -0500

    feat: :test_tube: Configuracion SESSION

--- server/middleware/sessionConfig.js
+++ server/middleware/sessionConfig.js
@@ -1,11 +1,12 @@
 const session = require('express-session');
 
+
 // const SECRET_KEY =  'tu_clave_secreta_aqui'
 
 module.exports = session({
     secret: process.env.EXPRESS_SECRET_KEY,
-    resave: false,
-    saveUninitialized: false,
+    resave: true,
+    saveUninitialized: true,
     cookie: {
         secure: false, // Asegúrate de usar 'true' si estás en HTTPS
         maxAge: 1800000
