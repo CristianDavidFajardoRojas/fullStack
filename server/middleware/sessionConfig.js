8483a525a0ef0b690a7dbddd5942311c1bc9af04
cris-fr <cfajardorojas98@gmail.com>
2024-10-31 15:58:32 -0500

    feat: :test_tube: Session Config TRY

--- server/middleware/sessionConfig.js
+++ server/middleware/sessionConfig.js
@@ -1,16 +1,11 @@
 const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');
-
-const redisClient = redis.createClient();
 
 // const SECRET_KEY =  'tu_clave_secreta_aqui'
 
 module.exports = session({
-    store: new RedisStore({ client: redisClient }),
     secret: process.env.EXPRESS_SECRET_KEY,
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
     cookie: {
         secure: false, // Asegúrate de usar 'true' si estás en HTTPS
         maxAge: 1800000
