39ccdef3ef77023cf6f024c981b41ffa6fdf6912
cris-fr <cfajardorojas98@gmail.com>
2024-11-01 07:22:04 -0500

    feat: :sparkles: Limite Peticiones

--- server/router/noteRouter.js
+++ server/router/noteRouter.js
@@ -7,6 +7,7 @@ const layout = require('../view/notesView');
 const layoutEdit = require('../view/editnoteView');
 const layoutSearch = require('../view/searchView');
 const layoutAdd = require('../view/addnoteView');
+const {limit} = require('../middleware/limit');
 
 
 
@@ -14,33 +15,33 @@ const layoutAdd = require('../view/addnoteView');
  * GET/ Note
  * ! Versiones 1.0.0
  */
-router.get("/:id/history", versionMiddleware("1.0.0"), historyController.findNoteChangeHistory);
+router.get("/:id/history", limit('get'), versionMiddleware("1.0.0"), historyController.findNoteChangeHistory);
 
-router.get("/:id", versionMiddleware("1.0.0"), noteController.findNoteById);
+router.get("/:id", limit('get'), versionMiddleware("1.0.0"), noteController.findNoteById);
 
-router.get("/", versionMiddleware("1.0.0"), noteController.findAllNotes);
+router.get("/", limit('get'), versionMiddleware("1.0.0"), noteController.findAllNotes);
 
 /**
  * POST/ Note
  * ! Versiones 1.0.0
  */
-router.post("/search", versionMiddleware("1.0.0"), noteController.findNotesMatchingTitleOrDescription);
+router.post("/search", limit('post'), versionMiddleware("1.0.0"), noteController.findNotesMatchingTitleOrDescription);
 
-router.post("/:id/history", versionMiddleware("1.0.0"), historyController.save);
+router.post("/:id/history", limit('post'), versionMiddleware("1.0.0"), historyController.save);
 
-router.post("/", versionMiddleware("1.0.0"), noteController.save);
+router.post("/", limit('post'), versionMiddleware("1.0.0"), noteController.save);
 
 /**
  * PUT/ Note
  * ! Versiones 1.0.0
  */
-router.put("/:id", versionMiddleware("1.0.0"), noteController.updateNoteById);
+router.put("/:id", limit('put'), versionMiddleware("1.0.0"), noteController.updateNoteById);
 
 /**
  * DELETE/ Note
  * ! Versiones 1.0.0
  */
-router.delete("/:id", versionMiddleware("1.0.0"), noteController.deleteNoteById);
+router.delete("/:id", limit('delete'), versionMiddleware("1.0.0"), noteController.deleteNoteById);
 
 router.use(layout);
 router.use('/addNote', layoutAdd);
--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -4,6 +4,7 @@ const versionMiddleware = require('../middleware/versionate');
 const layoutOpenning = require('../view/userView');
 const layoutLogin = require('../view/loginView');
 const layoutSignUp = require('../view/signupView');
+const {limit} = require('../middleware/limit');
 
 // Las rutas que preceden a otras rutas tienen prioridad.
 // Las rutas con parámetros, seguidas por otra ruta, ocupan el segundo lugar.
@@ -14,11 +15,11 @@ router.use(layoutOpenning);
 router.use('/login', layoutLogin);
 router.use('/signup', layoutSignUp);
 
-router.post("/login", versionMiddleware("1.0.0"), userController.signInUser);
-router.post("/logout", versionMiddleware("1.0.0"), userController.logOutUser);
-router.post("/", versionMiddleware("1.0.0"), userController.addNewUser);
+router.post("/login", limit('post', '/login'), versionMiddleware("1.0.0"), userController.signInUser);
+router.post("/logout", limit('post'), versionMiddleware("1.0.0"), userController.logOutUser);
+router.post("/", limit('post'), versionMiddleware("1.0.0"), userController.addNewUser);
 
-router.put("/:id", versionMiddleware("1.0.0"), userController.updateUserById);
-router.delete("/:id", versionMiddleware("1.0.0"), userController.deleteUserById);
+router.put("/:id", limit('put'), versionMiddleware("1.0.0"), userController.updateUserById);
+router.delete("/:id", limit('delete'), versionMiddleware("1.0.0"), userController.deleteUserById);
 
 module.exports = router;
